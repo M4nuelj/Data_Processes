ce3c93db5c347feabfd55d803cd40053e6539ace
Juan felipe Renza Chavarria <j.renza@solvoglobal.local>
2023-06-29 15:36:02 -0500

    Returns code can write item type and lot by itself

--- DayWeek.py
+++ DayWeek.py
@@ -1,14 +1,16 @@
 import datetime
-import pandas as pd
+
+#These 3 functions are able indentify which is the Lot of a product only 
+# if the Pack Date is given
 
 def get_week_of_year(date_str):
-    date_obj = datetime.datetime.strptime(date_str, '%m/%d/%Y')
+    date_obj = datetime.datetime.strptime(date_str, '%Y-%m-%d')
     week_number = date_obj.isocalendar()[1]
     return week_number
 
 
 def get_day_of_week(date_str):
-    date_obj = datetime.datetime.strptime(date_str, '%m/%d/%Y')
+    date_obj = datetime.datetime.strptime(date_str, '%Y-%m-%d')
     day_number = date_obj.weekday() + 2
     return day_number
 
@@ -16,11 +18,4 @@ def get_day_of_week(date_str):
 def get_lot_from_date(date_str): 
     return f'{get_week_of_year(date_str)}{get_day_of_week(date_str)}'
 
-# Example usage
-dates = ['06/28/2023', '06/29/2023']
-Disposals = pd.DataFrame()
-Disposals['Dates'] = dates
-result = list(map(lambda x : get_lot_from_date(x), Disposals.Dates))
-Disposals['Lot'] = result
-print(Disposals)
 
--- DisposalsTransfersMelt.py
+++ DisposalsTransfersMelt.py
@@ -1,46 +1,60 @@
 import pandas as pd
 import datetime
+from DayWeek import get_day_of_week, get_week_of_year, get_lot_from_date
 
-path1 = 'C:/Users/j.renza/Documents/Returns/'
+import warnings
 
-df= pd.read_excel(path1 + 'Disposal & Transfer Log (Responses).xlsx',
+warnings.filterwarnings("ignore")
+
+# These are the libraries that will be used in this code
+
+path = 'C:/Users/j.renza/Documents/Returns/'
+# path is the location where the files are kept. It is important that the files named
+# "Disposal & Transfer Log (Responses)" and "Products"
+# are within the same folder as they are both called using the same path
+
+df= pd.read_excel(path + 'Disposal & Transfer Log (Responses).xlsx',
                     usecols= ['Type of Return', 'Inspection Date', 'Transfer No.', 'Item',
-                     'Quantity (CS)', 'Quantity (Lbs)', 'Pack Date','Lot', 'Vendor',
+                     'Quantity (CS)', 'Quantity (Lbs)', 'Pack Date', 'Vendor',
                      'Reason', 'Comments'])
 
-df2 = pd.read_excel(path1 + 'Products.xlsx')
+# df is a DataFrame created from the export of the Google From found in the following link:
+# https://docs.google.com/forms/d/e/1FAIpQLSelAu5GL7vi91SIpMNKC60faZtV_Ei5RJmTBmEJcuqGGQV82Q/viewform
+
+df2 = pd.read_excel(path + 'Products.xlsx')
+
+#df2 is a DataFrame created from an excel file that contains all the items that PrimeMeats has available
+# The products are pulled from an external file instead of being named within the same Google Form as a
+# way of security... Items can change name;therefore, it'll be easier to re-write the entire file if the names are
+# kept in a different souce.
 
 df3 = df.merge(df2, left_on = 'Item', right_on = 'Item_Code')
 
-def get_week_of_year(date_str):
-    date_obj = datetime.datetime.strptime(date_str, '%m/%d/%Y')
-    week_number = date_obj.isocalendar()[1]
-    return week_number
-
-def get_day_of_week(date_str):
-    date_obj = datetime.datetime.strptime(date_str, '%m/%d/%Y')
-    day_number = date_obj.weekday() + 2
-    return day_number
-
-def get_lot_from_date(date_str):
-    return f'{get_week_of_year(date_str)}{get_day_of_week(date_str)}'
-
+# df3 is a DataFrame that contains the combination of df and df2. The merge
+# is done with the item as a key; therefore, if written information 
+# cannot be found in the final file the Products.xlsx file should be inspected.
 
 Disposals = df3[df3['Type of Return'] == 'Disposal']
 
-Disposals.columns = ['Type', 'Date', 'Transfer', 'Item', 'CS', 'Lbs', 'PackDate', 'Lot', 'Vendor', 'Reason', 'Comments', 'Item_Code', 'Item_Name']
+Disposals.columns = ['Type', 'Date', 'Transfer', 'Item', 'CS', 'Lbs', 'PackDate', 'Vendor', 'Reason', 'Comments', 'Item_Code', 'Item_Name', 'Item_Type_Code', 'Item_Type']
 
 Transfers = df3[df3['Type of Return'] == 'Transfer']
 
-Transfers.columns = ['Type', 'Date', 'Transfer', 'Item', 'CS', 'Lbs', 'PackDate', 'Lot', 'Vendor', 'Reason', 'Comments', 'Item_Code', 'Item_Name']
+Transfers.columns = ['Type', 'Date', 'Transfer', 'Item', 'CS', 'Lbs', 'PackDate', 'Vendor', 'Reason', 'Comments', 'Item_Code', 'Item_Name', 'Item_Type_Code', 'Item_Type']
 
-newDisposals = [Lot if Lot != 531.0 else 'N/A' for Lot in Disposals.Lot]
 
-newTransfers = [Lot if Lot != 531 else 'N/A' for Lot in Transfers.Lot]
+Disposals['PackDate'] = Disposals.PackDate.astype('str')
+Transfers['PackDate'] = Transfers.PackDate.astype('str')
 
-newDisposalsPackDate = [PackDate if PackDate != 1/1/2000 else 'N/A' for PackDate in Disposals.PackDate]
 
-newTransfersPackDate = [PackDate if PackDate != 1/1/2000 else 'N/A' for PackDate in Transfers.PackDate]
+Disposals['Lot'] = Disposals.apply(lambda row: get_lot_from_date(row['PackDate']) if row['Item_Type'] != 'BIBO' else 'N/A', axis = 1)
+
+Transfers['Lot'] = Transfers.apply(lambda row: get_lot_from_date(row['PackDate']) if row['Item_Type'] != 'BIBO' else 'N/A', axis = 1)
+
+
+Disposals['NewPackDate'] = Disposals.apply(lambda row: row['PackDate'] if row['PackDate'] != '1/1/2000' else 'N/A', axis = 1)
+
+Transfers['NewPackDate'] = Transfers.apply(lambda row: row['PackDate'] if row['PackDate'] != '1/1/2000' else 'N/A', axis = 1)
 
 DisposalsDetails = {'Date': Disposals.Date,
                     'Item': Disposals.Item,
@@ -56,8 +70,8 @@ DisposalsDetails = {'Date': Disposals.Date,
                     'WH90': "X",
                     'CS': Disposals.CS,
                     'Weight': Disposals.Lbs,
-                    'PackDate': newDisposalsPackDate,
-                    'Lot': newDisposals,
+                    'PackDate': Disposals.NewPackDate,
+                    'Lot': Disposals.Lot,
                     'Reason': Disposals.Reason,
                     'Comments': Disposals.Comments,
                     'TransferNo': Disposals.Transfer}
@@ -77,8 +91,8 @@ TransfersDetails = {'Date': Transfers.Date,
                     'WH90': "",
                     'CS': Transfers.CS,
                     'Weight': Transfers.Lbs,
-                    'PackDate': newTransfersPackDate,
-                    'Lot': newTransfers,
+                    'PackDate': Transfers.NewPackDate,
+                    'Lot': Transfers.Lot,
                     'Reason': Transfers.Reason,
                     'Comments': Transfers.Comments,
                     'TransferNo': Transfers.Transfer}
@@ -95,11 +109,8 @@ FinalTransfers.Date = FinalTransfers.Date.dt.strftime('%m/%d/%Y')
 
 FinalTransfers.PackDate = FinalTransfers.PackDate.dt.strftime('%m/%d/%Y')
 
-FinalDisposals['Lot2'] = list(map(lambda x : get_lot_from_date(x), FinalDisposals.PackDate)) 
-FinalTransfers['Lot2'] = list(map(lambda x : get_lot_from_date(x), FinalTransfers.PackDate))
-
-print(('File saved sucessfully'))
-
-with pd.ExcelWriter(path1 + 'Disposals & Transfers.xlsx') as writer:
+with pd.ExcelWriter(path + 'Disposals & Transfers.xlsx') as writer:
     FinalDisposals.to_excel(writer, sheet_name = 'Disposals', index = False)
     FinalTransfers.to_excel(writer, sheet_name = 'Transfers', index = False)
+
+print(('File saved sucessfully'))
