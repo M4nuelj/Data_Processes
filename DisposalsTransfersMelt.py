718d6f03fccf93a49e16540ebc706db179c2ef3c
QAPrimeMeats <quality@primemeats.com>
2023-06-30 10:06:40 -0500

    All bugs were fixed

--- DisposalsTransfersMelt.py
+++ DisposalsTransfersMelt.py
@@ -18,7 +18,7 @@ df= pd.read_excel(path + 'Disposal & Transfer Log (Responses).xlsx',
                      'Quantity (CS)', 'Quantity (Lbs)', 'Pack Date', 'Vendor',
                      'Reason', 'Comments'])
 
-# df is a DataFrame created from the export of the Google From found in the following link:
+# df is a DataFrame created from the export of the Google Form found in the following link:
 # https://docs.google.com/forms/d/e/1FAIpQLSelAu5GL7vi91SIpMNKC60faZtV_Ei5RJmTBmEJcuqGGQV82Q/viewform
 
 df2 = pd.read_excel(path + 'Products.xlsx')
@@ -34,6 +34,9 @@ df3 = df.merge(df2, left_on = 'Item', right_on = 'Item_Code')
 # is done with the item as a key; therefore, if written information 
 # cannot be found in the final file the Products.xlsx file should be inspected.
 
+
+# All the code below cleans the data and organize it in a way that match the SQF format.
+
 Disposals = df3[df3['Type of Return'] == 'Disposal']
 
 Disposals.columns = ['Type', 'Date', 'Transfer', 'Item', 'CS', 'Lbs', 'PackDate', 'Vendor', 'Reason', 'Comments', 'Item_Code', 'Item_Name', 'Item_Type_Code', 'Item_Type']
@@ -42,17 +45,12 @@ Transfers = df3[df3['Type of Return'] == 'Transfer']
 
 Transfers.columns = ['Type', 'Date', 'Transfer', 'Item', 'CS', 'Lbs', 'PackDate', 'Vendor', 'Reason', 'Comments', 'Item_Code', 'Item_Name', 'Item_Type_Code', 'Item_Type']
 
-
 Disposals['PackDate'] = Disposals.PackDate.astype('str')
 Transfers['PackDate'] = Transfers.PackDate.astype('str')
-
 
 Disposals['Lot'] = Disposals.apply(lambda row: int(get_lot_from_date(row['PackDate'])) if row['Item_Type'] != 'BIBO' else 'N/A', axis = 1)
 
 Transfers['Lot'] = Transfers.apply(lambda row: int(get_lot_from_date(row['PackDate'])) if row['Item_Type'] != 'BIBO' else 'N/A', axis = 1)
-
-
-# print(Disposals.PackDate)
 
 Disposals['NewPackDate'] = Disposals.apply(lambda row: row['PackDate'] if row['PackDate'] != '2000-01-01' else 'N/A', axis = 1)
 
@@ -103,16 +101,23 @@ FinalDisposals = pd.DataFrame(DisposalsDetails)
 
 FinalTransfers = pd.DataFrame(TransfersDetails)
 
-FinalDisposals['PackDate'] = FinalDisposals.PackDate.astype('datetime64[ns]')
-FinalTransfers['PackDate'] = FinalTransfers.PackDate.astype('datetime64[ns]')
-
 FinalDisposals.Date = FinalDisposals.Date.dt.strftime('%m/%d/%Y')
-
-FinalDisposals.PackDate = FinalDisposals.PackDate.dt.strftime('%m/%d/%Y')
 
 FinalTransfers.Date = FinalTransfers.Date.dt.strftime('%m/%d/%Y')
 
-FinalTransfers.PackDate = FinalTransfers.PackDate.dt.strftime('%m/%d/%Y')
+
+FinalDisposals['PackDate'] = pd.to_datetime(FinalDisposals['PackDate'], errors='coerce')
+FinalDisposals['PackDate'] = FinalDisposals['PackDate'].dt.strftime('%m/%d/%Y')
+FinalDisposals.loc[pd.isna(FinalDisposals['PackDate']), 'PackDate'] = 'N/A'
+
+
+FinalTransfers['PackDate'] = pd.to_datetime(FinalTransfers['PackDate'], errors='coerce')
+FinalTransfers['PackDate'] = FinalTransfers['PackDate'].dt.strftime('%m/%d/%Y')
+FinalTransfers.loc[pd.isna(FinalTransfers['PackDate']), 'PackDate'] = 'N/A'
+
+# The code below saves both Dataframes with the disposals and Transfers Information into a new excel file
+# that is called 'Disposals & Transfers'
+
 
 with pd.ExcelWriter(path + 'Disposals & Transfers.xlsx') as writer:
     FinalDisposals.to_excel(writer, sheet_name = 'Disposals', index = False)
